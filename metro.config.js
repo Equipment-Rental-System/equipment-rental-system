39a225eb21681151f65c72ea787f16ca953a82ac
breezemin1009 <jhm0112992902@gmail.com>
2026-05-02 15:00:27 +0900

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver.blockList =
-  /android[\\/]\.gradle[\\/].*|android[\\/]build[\\/].*|android[\\/]app[\\/]build[\\/].*|backend[\\/]uploads[\\/].*|database[\\/].*|\.expo[\\/].*/;
-
-module.exports = config;
